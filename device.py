4b45fff2cbd50fb98f6c10423f7c8337058eb36e
mobilteknolojileri <mobilteknolojileri@gmail.com>
2025-12-11 23:28:09 +0300

    feat: add device ID generator

---
+++ device.py
@@ -0,0 +1,8 @@
+import hashlib
+import random
+import time
+
+def generate_device_id():
+    random_data = f"{random.random()}-{time.time()}"
+    device_id = hashlib.sha1(random_data.encode('utf-8')).hexdigest().upper()
+    return device_id
