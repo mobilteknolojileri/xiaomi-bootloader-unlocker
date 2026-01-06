a7abce812e254106c1000c6a0ab173e08a3032bd
mobilteknolojileri <mobilteknolojileri@gmail.com>
2026-01-06 18:58:41 +0300

    refactor: add type hints and docstrings to all modules

--- device.py
+++ device.py
@@ -1,8 +1,19 @@
+"""
+Device ID generation module.
+"""
+
 import hashlib
 import random
 import time
 
-def generate_device_id():
+
+def generate_device_id() -> str:
+    """
+    Generate a unique device ID using SHA1 hash.
+    
+    Returns:
+        str: A 40-character uppercase hexadecimal device ID.
+    """
     random_data = f"{random.random()}-{time.time()}"
     device_id = hashlib.sha1(random_data.encode('utf-8')).hexdigest().upper()
     return device_id
